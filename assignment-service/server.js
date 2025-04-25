2ad8618dea6e4bbd37883b5204128a53edf2dcf5
JBB0807 <104856796+JBB0807@users.noreply.github.com>
2025-04-25 11:07:46 -0700

    code cleanup

--- assignment-service/server.js
+++ assignment-service/server.js
@@ -1,37 +1,13 @@
 require('dotenv').config();
 
-const cors = require("cors");
 const express = require("express");
-const passport = require("passport");
-const passportSetup = require("./passport");
-const authRoute = require("./routes/auth");
-const session = require("express-session");
+const instructorRouter = require("./routes/InstructorRouter");
+const studentRouter = require("./routes/StudentRouter");
 
 const app = express();
 
-app.use(
-    session({
-        secret: process.env.AUTH_SESSION_KEY,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            maxAge: 24 * 60 * 60 * 1000, // 1 day
-        },
-    })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(
-    cors({
-        origin: "https://bytecamp-web.fly.dev",
-        methods: "GET",
-        credentials: true,
-    })
-)
-
-app.use("/auth", authRoute);
+app.use("/instructor", instructorRouter);
+app.use("/student", studentRouter);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
