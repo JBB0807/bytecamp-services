27abb409c089b1c03868db2f19390a31ef36335e
JBB0807 <104856796+JBB0807@users.noreply.github.com>
2025-05-21 11:16:29 -0700

    performance improvement on proxy service

--- proxy-service/server.js
+++ proxy-service/server.js
@@ -4,31 +4,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 8080;
 
-// Middleware to handle dynamic IPv6 proxying
-// app.use('/:ipv6', (req, res, next) => {
-//   const ipv6 = req.params.ipv6;
-
-//   // Validate or sanitize the IPv6 if needed
-//   const targetUrl = `http://[${ipv6}]:8000`;
-//   console.log(`Proxying request to: ${targetUrl}`);
-
-//   // Create and attach the proxy middleware *once per request*
-//   const proxy = createProxyMiddleware({
-//     target: targetUrl,
-//     changeOrigin: true,
-//     logLevel: 'debug',
-//     // pathRewrite: {
-//     //   [`^/${ipv6}`]: '/', // Send to root of the target
-//     // },
-//     onError(err, req, res) {
-//       console.error('Proxy error:', err.message);
-//       res.status(502).send('Bad Gateway: Failed to connect to target');
-//     }
-//   });
-
-//   return proxy(req, res, next);
-// });
-
 // Cache proxy instances per target to avoid recreating each time
 const proxyCache = new Map();
 
